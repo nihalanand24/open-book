5f2a24cdef0a2138d3c89cc32f8ebb40d27e4a47
nihalanand24 <nihalanand@gmail.com>
2021-07-25 19:12:35 -0400

    core functionality completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getCoverImage } from "./api/endpoints";
 import getBookResults from "./api/getBookResults";
 import "./App.scss";
 
 function App() {
   const [bookTitle, setBookTitle] = useState("");
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [searchFlag, setSearchFlag] = useState(false);
+  const [originalSortOrder, setOriginalSortOrder] = useState([]);
 
   const handleChange = (event) => {
+    setSearchFlag(false);
     setBookTitle(event.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    getBookResults(bookTitle, setResults);
+    setLoading(true);
+    getBookResults(bookTitle, setResults, setLoading, setFailure, setSearchFlag, setOriginalSortOrder);
   };
+
+
+  const sortAlphabetically = () => {
+    const books = [...results];
+    const sortedBooks = books.sort((a, b) => a.title.localeCompare(b.title));
+    setResults(sortedBooks);
+  }
+
+  const sortByYearPublished = () => {
+    const books = [...results];
+    const sortedBooks = books.sort((a, b) => b.first_publish_year - a.first_publish_year);
+    setResults(sortedBooks);
+  }
+
+  const getAuthorsListString = authorsArray => {
+    if (authorsArray.length > 1) {
+      let authorsString = '';
+      authorsArray.forEach((author, index) => {
+        switch (index) {
+          case 0:
+            authorsString += author;
+            break;
+          case authorsArray.length - 1:
+            authorsString += ` & ${author}`;
+            break;
+          default:
+            authorsString += `, ${author}`
+            break;
+        }
+      })
+      return authorsString;
+    } else {
+      return authorsArray[0];
+    }
+  }
+
 
   return (
     <div className="App">
@@ -25,18 +67,34 @@ function App() {
           <button type="submit">Search</button>
         </form>
 
-        <div className="bookResults">
-          {results && results.map(book => (
-            <div className="bookCard">
-              <p>{book.title}</p>
-              <div className="bookCover">
-                <img src={getCoverImage(book.cover_i, "l")} alt={`Book Cover for ${book.title}`} />
-              </div>
-            </div>
-          ))
 
-          }
-        </div>
+        <button onClick={sortAlphabetically}>Sort Alphabetically {}</button>
+        <button onClick={sortByYearPublished}>Sort by Year</button>
+        <button onClick={() => setResults(originalSortOrder)}>Remove Custom Sort</button>
+        {loading ? <p className="loading">Loading Results</p>
+          : failure ? <p>Error Loading Data from API. Please try later.</p> 
+          : results.length ? 
+          <div className="bookResults">
+            {results.map(book => (
+              <div key={book.key} className="bookCard">
+                <p>{book.title}</p>
+                <p>{book.first_publish_year}</p>
+                <p>By: {getAuthorsListString(book.author_name)}</p>
+                <div className="bookCover">
+                  {book.cover_i ? <img 
+                    src={getCoverImage(book.cover_i, "m")} 
+                    alt={`Book Cover for ${book.title}`} 
+                    />
+                  : <div className="noImage">
+                      <p>No image available</p>
+                    </div> 
+                  }
+                </div>
+              </div>
+            ))}
+          </div>
+          : searchFlag && <p>No Results Found for {bookTitle}</p>
+        }
 
       </div>
     </div>
--- src/api/getBookResults.jsx
+++ src/api/getBookResults.jsx
@@ -1,11 +1,21 @@
 import axios from "axios"
 import { titleBaseUrl } from "./endpoints"
 
-const getBookResults = async (title, setResults) => {
-    
-    const response = await axios(`${titleBaseUrl}${title}`);
-    const results = response.data.docs;
-    setResults(results.slice(0, 15));
+const getBookResults = async ( title, setResults, setLoading, setFailure, setSearchFlag, setOriginalSortOrder ) => {
+    setSearchFlag(false);
+    try {
+        const response = await axios(`${titleBaseUrl}${title}`);
+        const results = response.data.docs;
+        setResults(results.slice(0, 15));
+        setLoading(false);
+        setFailure(false);
+        setOriginalSortOrder(results.slice(0, 15));
+    } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setFailure(true);
+    }
+    setSearchFlag(true);
 
 }
 
